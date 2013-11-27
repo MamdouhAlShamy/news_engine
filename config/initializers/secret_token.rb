8f2232cecc219e9a99ab2347c1b3ba65080a74bd
mms <mms@mms-Latitude-E5500.(none)>
2013-11-27 11:25:39 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+News::Application.config.secret_key_base = '3ba0b682f30929712cd6beb0cbb3c6bfc727d2d30558db3c79871b83f47ec060eab656c77fc30a4105c639ea5036296e76c22b84171ec101d0fdf77d96fa032e'
