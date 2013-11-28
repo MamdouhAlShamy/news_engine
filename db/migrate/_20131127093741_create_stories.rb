0745b024e8076d472698a132632f9a6262b65d75
mms <mms@mms-Latitude-E5500.(none)>
2013-11-28 17:44:21 +0200

    create model locally

---
+++ db/migrate/_20131127093741_create_stories.rb
@@ -0,0 +1,16 @@
+class CreateStories < ActiveRecord::Migration
+  def change
+    create_table :stories do |t|
+      t.integer :story_id
+      t.integer :provider_id
+      t.integer :category_id
+      t.text :title
+      t.text :details
+      t.string :img
+      t.time :time
+      t.string :url
+
+      t.timestamps
+    end
+  end
+end
