b5ccd4a21c96c0a6d6953381453a752cd672e5d3
mamdouhalshamy <mamdouh.al.shamy@gmail.com>
2014-01-03 15:34:16 +0200

    trial to fix Heroku refusal to POST

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-News::Application.config.session_store :cookie_store, key: '_news_session'
+News::Application.config.session_store :cookie_store, key: '_news_session', domain: :all
